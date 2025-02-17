b181fe94c4001cfe7a24eec82d784df62a89e411
Christofer Hansson <christofer_hansson@hotmail.com>
2025-02-17 18:01:45 +0100

    Fått till filtrering och sort fallande!

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,33 @@
 "use strict";
 
 let tbodyEl = document.querySelector("tbody");
+let sort1El = document.querySelector("#sort1");
+let sort2El = document.querySelector("#sort2");
+let sort3El = document.querySelector("#sort3");
+
 let courses = [];
+
+
 
 window.onload = () => {
     getCourses();
+    document.querySelector("#search").addEventListener("input", filterCourses);
+    document.querySelector("#sort1").addEventListener("click", sortCourses);
+    document.querySelector("#sort2").addEventListener("click", sortCourses2);
+    document.querySelector("#sort3").addEventListener("click", sortCourses3);
+
 }
 
 
 async function getCourses() {
+    console.log(courses);
+
     try {
         const response = await fetch(
             "https://webbutveckling.miun.se/files/ramschema_ht24.json"
         );
 
-        const courses = await response.json();
+        courses = await response.json();
         console.log(courses);
         dataToTable(courses);
 
@@ -24,12 +37,45 @@ async function getCourses() {
 }
 
 
-function dataToTable(courses) {
+function dataToTable(data) {
 tbodyEl.innerHTML = "";
 
-    courses.forEach(course => {
+    data.forEach(course => {
         tbodyEl.innerHTML += `<tr><td>${course.code}</td><td>${course.coursename}</td><td>${course.progression}</td></tr>`;
     });
     console.log("Här fortsätter mitt program...");
 
+}
+
+function filterCourses() {
+
+    console.log("Här fortsätter mitt program 2...");
+    console.log(courses);
+
+    const searchInput = document.querySelector("#search").value.toLowerCase();
+    const filteredCourses = courses.filter((course) => 
+        course.code.toLowerCase().includes(searchInput) ||
+        course.coursename.toLowerCase().includes(searchInput)
+    );
+    console.log(filteredCourses);
+    dataToTable(filteredCourses);
+
+}
+
+function sortCourses() {
+    const sorted1 = courses.sort((a, b) => a.code > b.code ? 1 : -1);
+    console.table(courses);
+    dataToTable(sorted1);
+}
+
+function sortCourses2() {
+    const sorted2 = courses.sort((a, b) => a.coursename > b.coursename ? 1 : -1);
+    console.table(courses);
+    dataToTable(sorted2);
+}
+
+function sortCourses3() {
+    const sorted3 = courses.sort((a, b) => a.progression > b.progression ? 1 : -1);
+    console.table(courses);
+    dataToTable(sorted3);
 }
